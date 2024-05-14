0b369ec0dc69d8e6a51cb6d71e992da544b7b04f
Patrick Podbregar <patrick.podbregar@gmail.com>
2024-05-14 16:44:24 +0200

    Use .zshrc from .dotfiles

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # Check for Homebrew and install it if not available
-if [ ! $(which brew) ]; then
+if [[ ! $(which brew) ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
