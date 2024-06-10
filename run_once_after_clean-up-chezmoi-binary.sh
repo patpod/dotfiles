2680e3d2fb36d151da29c460e410c844cc30d2b8
Patrick Podbregar <patrick.podbregar@gmail.com>
2024-06-10 21:10:06 +0200

    Fix typo in script

--- run_once_after_clean-up-chezmoi-binary.sh
+++ run_once_after_clean-up-chezmoi-binary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# When using the one-line bootstrap nstallation chezmoi is installed in the 
+# When using the one-line bootstrap installation chezmoi is installed in the 
 # ~/bin folder. Since we use Homebrew to install it afterwards we can clean
 # up this executable
 CHEZMOI="~/bin/chezmoi"
